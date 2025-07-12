dd025db62550ae0e8c489a290ee55f21062a9de2
Jivan Bal <bal.jivan@yahoo.com>
2025-07-12 03:05:29 -0700

    all test cases passed

--- Homework/hw2/hw2.py
+++ Homework/hw2/hw2.py
@@ -39,32 +39,86 @@ length_question = "Enter a number to view songs by length. (Positive: longest so
 length_value_error = "Invalid vallue. Please enter a number."
 
 # CODE START
+def main ():
+    state = True;
+    while state:
+        user = input(user_choice_question)
 
-user = input(user_choice_question)
+        if (user == "1"):
+            empty = set();
+            for i in spotify:
+                for art in spotify[i]["artists"]:
 
-if (user == "1"):
-    empty = set();
-    for i in spotify:
-        for art in spotify[i]["artists"]:
+                    if art not in empty:
+                        empty.add(art)
+            
+            print(", ".join(sorted(empty)))
+            state = False;
+            
 
-            if art not in empty:
-                empty.add(art)
-    
-    print(", ".join(sorted(empty)))
+        elif (user == "2"):
+            user2 = input("Enter the ranking you're interested in (between 1 and 10) : ")
 
-elif (user == "2"):
-    user2 = (input("Enter the ranking you're interested in (between 1 and 10) : "))
+            if (not user2.isdigit()):
+                print("Invalid input. Please enter a number.")
+                state = False;
+            
+            elif (int(user2) < 0 or int(user2) > 11):
+                print("Ranking out of range.")
+                state = False;
+            
+            else:
+                print(f"{int(user2)}: {spotify[int(user2)]['title']} by {', '.join(spotify[int(user2)]['artists'])}")
+                state = False;
 
-    if (not user2.isdigit()):
-        print("Invalid input. Please enter a number.")
-    
-    elif (int(user2) < 0 or int(user2) > 11):
-        print("Ranking out of range.")
-    
-    else:
-        print(f"{int(user2)}: {spotify[int(user2)]['title']} by {', '.join(spotify[int(user2)]['artists'])}")
 
-else:
-    print("Error")
-    
+        elif (user == "3"):
+            user3 = input("Enter the name of the artist you're interested in: ")
 
+            count = 0
+
+            for i in spotify:
+                for person in spotify[i]["artists"]:
+                    if (person.lower() == user3.lower()):
+                        print(f"{i}: {spotify[i]['title']}")
+                        count+=1
+                state = False;
+            if (count == 0):
+                print(f"No songs were found by {user3}")
+                state = False;
+
+        elif (user == "4"):
+            user4 = input("Enter a number to view songs by length. (Positive: longest songs, Negative: shortest songs): ")
+
+            if (user4.isdigit() or int(user4)):
+                if (int(user4) > 0):
+                    list1 = (list(spotify.values()))
+                    s = sorted(list1, key=lambda item: item['length'], reverse=True)
+                    for item in s[:int(user4)]:
+                        min, sec = item["length"].split(":")
+                        seconds = (int(min) * 60) + int(sec)
+                        print(f"{item['title']} by {', '.join(item['artists'])} ({seconds} seconds)")
+                    
+                    state = False
+
+                elif (int(user4) < 0):
+                    list1 = (list(spotify.values()))
+                    s = sorted(list1, key=lambda item: item['length'], reverse=False)
+                    user4 = int(user4) * (-1)
+                    for item in s[:int(user4)]:
+                        min, sec = item["length"].split(":")
+                        seconds = (int(min) * 60) + int(sec)
+                        print(f"{item['title']} by {', '.join(item['artists'])} ({seconds} seconds)")
+                    
+                    state = False
+
+            else:
+               print("Invalid value. Please enter a number.")
+
+        elif (user == "0"):
+            exit()
+
+        else:
+            print("Error")
+
+main()
